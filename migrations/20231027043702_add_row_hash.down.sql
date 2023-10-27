60288317f72e0815c738f95600015f471635715e
Lynne Fuyuna <yryuu@outlook.com>
2023-10-27 13:36:27 +0800

    chore: add missing migrations

---
+++ migrations/20231027043702_add_row_hash.down.sql
@@ -0,0 +1,2 @@
+alter table customers drop column row_hash;
+alter table merchants drop column row_hash;
---
+++ migrations/20231027043702_add_row_hash.up.sql
@@ -0,0 +1,2 @@
+alter table customers add column row_hash bytea;
+alter table merchants add column row_hash bytea;
