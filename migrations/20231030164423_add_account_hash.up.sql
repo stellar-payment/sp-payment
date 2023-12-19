1525b4ae0f7b688ba01b2c8b55a589a381cf4d93
Lynne Fuyuna <yryuu@outlook.com>
2023-12-19 11:37:25 +0800

    fix: add conversion handler on migration

--- migrations/20231030164423_add_account_hash.up.sql
+++ migrations/20231030164423_add_account_hash.up.sql
@@ -1,3 +1,3 @@
-alter table accounts alter column account_no TYPE bytea;
+alter table accounts alter column account_no TYPE bytea using account_no::bytea;
 alter table accounts add column account_no_hash bytea not null default ''::bytea;
 alter table accounts add column row_hash bytea not null default ''::bytea;
